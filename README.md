97c477d7acdb5c4ac47585bded4481a1882f3830
Mauricio Payetta <mauricio.payetta@dianrong.com>
2015-09-18 12:58:47 +0800

    initial checkin

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cc-bmr-widget
+
+Simple widget to calculate calories consumption based on a person's gender, age, weight, height and activity level.
+
+It uses the English BMR formula to calculate the daily burned calories.
---
+++ widget.js
@@ -0,0 +1,143 @@
+var genderInput = document.getElementById("gender"),
+	ageInput = document.getElementById("age"),
+	weightInput = document.getElementById("weight"),
+	heightInput = document.getElementById("height"),
+	activityInput = document.getElementById("activity"),
+	caloriesInput = document.getElementById("dailyCalories");
+
+genderInput.onchange = function() {
+	triggerHarrisBenedictCalculation();
+};
+
+ageInput.onchange = function() {
+	checkIsNumber(ageInput);
+	triggerHarrisBenedictCalculation();
+};
+
+weightInput.onchange = function() {
+	checkIsNumber(weightInput);
+	triggerHarrisBenedictCalculation();
+};
+
+heightInput.onchange = function() {
+	checkIsNumber(heightInput);
+	triggerHarrisBenedictCalculation();
+};
+
+activityInput.onchange = function() {
+	triggerHarrisBenedictCalculation();
+};
+
+var checkIsNumber = function(input) {
+	if (isNaN(parseInt(input.value))) {
+		input.value = '';	
+	}
+}
+
+var hasValue = function(input) {
+	return (input.value && input.value.trim().length > 0);
+}
+
+var triggerHarrisBenedictCalculation = function() {
+	if (hasValue(ageInput) && hasValue(weightInput) && hasValue(heightInput)) {
+		var hbf = calculateHarrisBenedictFormula();
+		caloriesInput.value = hbf;
+	} else {
+		caloriesInput.value = '';
+	}
+}
+
+var FOOT_TO_INCHES = 12;
+var calculateBMR = function() {
+	var modifier = genderInput.value == 'female' ? 655 : 66;
+	var weightModifier = genderInput.value == 'female' ? 4.35 : 6.23;
+	var heightModifier = genderInput.value == 'female' ? 4.7 : 12.7;
+	var ageModifier = genderInput.value == 'female' ? 4.7 : 6.8;
+	
+	var heightInches = heightInput.value * FOOT_TO_INCHES;
+	
+	return modifier + (weightModifier * weightInput.value) + (heightModifier * heightInches) - (ageModifier * ageInput.value);
+}
+
+var calculateHarrisBenedictFormula = function() {
+	var bmr = calculateBMR();
+	var result = bmr * activityInput.value;
+	return result.toFixed(2);
+}
+
+
+var loseRadio = document.getElementById("lose"),
+	gainRadio = document.getElementById("gain"),
+	losePounds = document.getElementById("losePounds"),
+	loseDays = document.getElementById("loseDays"),
+	gainPounds = document.getElementById("gainPounds"),
+	gainDays = document.getElementById("gainDays"),
+	result = document.getElementById("result");
+
+losePounds.onchange = function() {
+	checkIsNumber(losePounds);
+	triggerCaloriesCalculation();
+};
+
+loseDays.onchange = function() {
+	checkIsNumber(loseDays);
+	triggerCaloriesCalculation();
+};
+
+gainPounds.onchange = function() {
+	checkIsNumber(gainPounds);
+	triggerCaloriesCalculation();
+};
+
+gainDays.onchange = function() {
+	checkIsNumber(gainDays);
+	triggerCaloriesCalculation();
+};
+
+loseRadio.onclick = function() {
+	selectLoseRadio();
+}
+
+var selectLoseRadio = function() {
+	losePounds.removeAttribute("disabled");
+	losePounds.focus();
+	loseDays.removeAttribute("disabled");
+	gainPounds.setAttribute("disabled", "true");
+	gainDays.setAttribute("disabled", "true");
+	gainPounds.value = '';
+	gainDays.value = '';
+	result.value = '';
+}
+
+gainRadio.onclick = function() {
+	gainPounds.removeAttribute("disabled");
+	gainPounds.focus();
+	gainDays.removeAttribute("disabled");
+	losePounds.setAttribute("disabled", "true");
+	loseDays.setAttribute("disabled", "true");
+	losePounds.value = '';
+	loseDays.value = '';
+	result.value = '';
+}
+
+var triggerCaloriesCalculation = function() {
+	if (loseRadio.checked) {
+		if (hasValue(losePounds) && hasValue(loseDays)) {
+			result.value = calculateNeededCalories(parseInt(losePounds.value), parseInt(loseDays.value));
+		}
+	} else if (gainRadio.checked) {
+		if (hasValue(gainPounds) && hasValue(gainDays)) {
+			result.value = calculateNeededCalories(parseInt(gainPounds.value), parseInt(gainDays.value));
+		}
+	}
+}
+
+var calculateNeededCalories = function(pounds, days) {
+	var val = pounds * 3500 / days;
+	return val.toFixed(2);
+}
+
+loseRadio.checked = true;
+selectLoseRadio();
+
+genderInput.focus();
